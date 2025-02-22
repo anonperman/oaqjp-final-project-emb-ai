ba1ab2b098e8cbd104da2f5e07b8caf0e95da053
I Dont Know <idont@know.com>
2025-02-22 18:02:16 -0500

    continue

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -11,11 +11,22 @@ import json
 ## object as received from the Emotion Detection function.
 ##
 def emotion_detector(text_to_analyze):
+
     URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     Headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(URL, json = input_json, headers=Headers)
     # print (json.dumps(response.text, indent=4))
+
+    if response.status_code == 400:
+        return json.dumps({
+                "anger": None,
+                "disgust": None,
+                "fear": None,
+                "joy": None,
+                "sadness": None,
+                "dominant_emotion": None
+        })
 
     resp_dict = json.loads(response.text)
     emotions_list = resp_dict["emotionPredictions"][0]["emotionMentions"][0]["emotion"]
