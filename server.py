8be58bb3c5b9649df782bcbc4d36c3db1dda6e4b
I Dont Know <idont@know.com>
2025-02-22 18:02:12 -0500

    continue

--- server.py
+++ server.py
@@ -14,7 +14,12 @@ def displayInputPage():
 
 @app.route("/emotionDetector")
 def getEmotion():
-    result = json.loads(emotion_detection.emotion_detector(request.args.get('textToAnalyze')))
+    emotion_str = emotion_detection.emotion_detector(request.args.get('textToAnalyze'))
+    result = json.loads(emotion_str)
+
+    if not result['dominant_emotion']:
+        return "Invalid text! Please try again!"
+
     result_str = "For the given statement, the system response is "
     result_str += "\"" + "anger" + "\": " + str(result["anger"])
     result_str += ", \"" + "disgust" + "\": " + str(result["disgust"])
