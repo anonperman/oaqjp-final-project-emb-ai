780fbb7736b60b48c67abe9b186f9b651637c3b1
I Dont Know <idont@know.com>
2025-02-22 17:34:32 -0500

    continue

--- server.py
+++ server.py
@@ -1,13 +1,12 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, render_template
 from flask import request
 from flask import Response
 from flask import redirect
+import json
 
 from EmotionDetection import emotion_detection
 
-
 app = Flask("Emotions detector")
-
 
 @app.route("/")
 def displayInputPage():
@@ -17,10 +16,14 @@ def displayInputPage():
 def getEmotion():
     result = json.loads(emotion_detection.emotion_detector(request.args.get('textToAnalyze')))
     result_str = "For the given statement, the system response is "
-    for emotion in emotions_test_dict.keys():
-        result_str += "\" + emotion + "\\\": " + float(result[expected_emotion]) + ","
-        self.assertEqual(expected_emotion, result['dominant_emotion'])    
+    result_str += "\"" + "anger" + "\": " + str(result["anger"])
+    result_str += ", \"" + "disgust" + "\": " + str(result["disgust"])
+    result_str += ", \"" + "fear" + "\": " + str(result["fear"]) + ", "
+    result_str += ", \"" + "joy" + "\": " + str(result["joy"]) + " and "
+    result_str += "\"" + "sadness" + "\": " + str(result["sadness"]) + ". "
+    result_str += "The dominant emotion is " + result['dominant_emotion'] + "."
 
+    return result_str
 
 if __name__ == "__main__":
     app.run()
